a7b716be82f4451a915bfd17831fa8ce8c19e4e1
Harry Muema <harymueema@gmail.com>
2022-07-18 14:58:45 +0300

    added the counters component to take multiple instances of counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,33 +2,34 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state={
-        count:0,
-        tags:["tag1", "tag2", "tag3"]
+        value:this.props.value,
+        // tags:["tag1", "tag2", "tag3"]
     }
     handleIncrement=product=>{
       console.log(product)
-      this.setState({count:this.state.count+1})
+      this.setState({value:this.state.value+1})
     }
   render() {
     return(
         <div>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm" onClick={()=>this.handleIncrement({id:1})}>Increment</button>
-            <ul>
+            <button onClick={()=>this.props.onDelete(this.props.id)} className="btn btn-danger m-2 btn-sm">Delete</button>
+            {/* <ul>
                 {this.state.tags.map(tag=><li key={tag}>{tag}</li>)}
-            </ul>
+            </ul> */}
         </div>
     );
   }
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        classes += (this.state.value === 0) ? 'warning' : 'primary';
         return classes;
     }
 
   formatCount(){
-    const {count}=this.state
-    return count===0?'Zero':count
+    const {value}=this.state
+    return value===0?'Zero':value
   }
 }
 
